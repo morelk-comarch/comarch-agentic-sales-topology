3183438ad4b87712a0f7040aff7c01df0dc2ee17
MarcinKruszynski <martin.kruszynski@comarch.com>
2025-12-04 12:53:48 +0000

    feat: add deploy file fix

--- src/deploy_to_agent_engine.sh
+++ src/deploy_to_agent_engine.sh
@@ -1,7 +1,7 @@
 adk deploy agent_engine \
 --project="prj-cmr-sbx-hackaton-1" \
 --region="us-central1" \
---staging_bucket="gs://emka-adk-training" \
---display_name="Sales agent preview 1" \
+--staging_bucket="gs://bucket-cmr-sbx-hackaton-1" \
+--display_name="Sales agent" \
 --env_file=".env" \
 sales_agent
